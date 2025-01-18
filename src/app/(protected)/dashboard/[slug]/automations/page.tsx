1b566232c83c12c6ef488530ac5b686a37f2ac30
Subham Kumar <53145627+subham0kumar@users.noreply.github.com>
2025-01-18 20:34:01 +0530

    Add automations page and components, update billing card labels, and refactor SVG icons

---
+++ src/app/(protected)/dashboard/[slug]/automations/page.tsx
@@ -0,0 +1,44 @@
+import AutomationsList from "@/components/Global/automations-list";
+import CreateAutomation from "@/components/Global/create-automation";
+import { Check } from "lucide-react";
+import React from "react";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  return (
+    <div className="grid grid-col-1 lg;grid-col-6 gap-5">
+      <div className="lg:col-span-4">
+        <AutomationsList />
+      </div>
+      <div className="lg:col-span-2">
+        <div className="flex flex-col rounded-xl bg-background-80 gap-y-6 p-5 border-[1px] border-in-active">
+          <div>
+            <h2 className="text-xl">Automations</h2>
+            <p className="text-text-secondary">
+              Your live automations will show here.
+            </p>
+          </div>
+          <div className="flex flex-col gap-y-3">
+            {[1, 2, 3].map((item) => (
+              <div key={item} className="flex items-start justify-between">
+                <div className="flex flex-col">
+                  <h3 className="font-medium">
+                    Direct traffic towards website
+                  </h3>
+                  <p className="text-text-secondary text-sm">
+                    October 5th 2024
+                  </p>
+                </div>
+                <Check />
+              </div>
+            ))}
+          </div>
+          <CreateAutomation />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
